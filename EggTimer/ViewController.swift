e70cced108f49c1986e7fee77ce3431063aa3eb7
jibralta <joyumali@gmail.com>
2016-07-25 19:47:33 -0700

    Timer code added along with UI. TO DO: Animate gif.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,10 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = Timer()     // Global variable.
+    var time = 0
+    
+    
+    @IBOutlet var timerLabel: UILabel!
+    
+    func processTimer() {
+        
+        time += 1 // one second is added each time.
+        timerLabel.text = "\(time)"
+        print("\(time)")
+    }
+    
 
+    @IBAction func startButton(_ sender: AnyObject) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func pauseButton(_ sender: AnyObject) {
+        
+        timer.invalidate()
+        
+    }
+    
+    @IBAction func resetButton(_ sender: AnyObject) {
+        
+        timer.invalidate()
+        time = 0
+        timerLabel.text = "0"
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+
+        
     }
 
     override func didReceiveMemoryWarning() {
